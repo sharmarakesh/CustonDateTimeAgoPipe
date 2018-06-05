d89cbed2e958eadc3c02377b2f4484694429687c
Rakesh Sharma <rakeshsharma.sharma@gmail.com>
2018-06-05 18:23:10 +0530

    Update rating-service.ts

--- src/app/services/rating-service.ts
+++ src/app/services/rating-service.ts
@@ -8,6 +8,16 @@ export class RatingService {
   private navigationList: any = [];
   private productList: any = [];
 
+  // private currentTab = new Subject<any>();
+
+  // public getCurrentTab() {
+  //   return this.currentTab.asObservable();
+  // }
+
+  // public setCurrentTab(data) {
+  //   this.currentTab.next(data);
+  // }
+
   private subject = new Subject<any>();
   private tabChange = new Subject<string>();
 
@@ -53,12 +63,13 @@ export class RatingService {
       const id1: string = id.toString();
       data.id = id1;
       console.log('TIME STAMP :', id1);
-      this.setProductList(data);
       console.log('PRODUCT LIST : ', this.getProductList());
       const navi = this.navigationService.getNavigationByType(data.label);
       console.log('FILTERED NAVIGATION : ', navi);
       navi.identificationNo = id1;
+      data.navigationlist = navi;
       this.setNavigationList(navi);
+      this.setProductList(data);
       this.subject.next(data);
   }
 }
