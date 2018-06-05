3c039c947e2dfc6f515a640c9dd02824b25a25d2
Rakesh Sharma <rakeshsharma.sharma@gmail.com>
2018-06-05 18:24:39 +0530

    Update navigation-bar.component.ts

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -8,12 +8,16 @@ import { RatingService } from '../services/rating-service';
 })
 export class NavigationBarComponent implements OnInit {
   navigationList: any;
+  currentTab;
   constructor(private ratingService: RatingService) { }
 
   ngOnInit() {
    // this.navigationList = this.ratingService.getNavigationList().slice();
     this.ratingService.getMessage().subscribe(() => {
       this.navigationList = this.ratingService.getNavigationList().slice();
+    });
+    this.ratingService.getTabChange().subscribe((data) => {
+      this.currentTab = data;
     });
   }
 
